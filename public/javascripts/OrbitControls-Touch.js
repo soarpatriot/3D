0819f2268941deb75d65aecc928a1993385364a8
Soar <85624529@qq.com>
2013-10-24 23:39:43 +0800

    fix when mouse wheel scroll brower scroll bar scrolling

--- public/javascripts/OrbitControls-Touch.js
+++ public/javascripts/OrbitControls-Touch.js
@@ -348,6 +348,14 @@ THREE.OrbitControls = function ( object, domElement ) {
 
 	function onMouseWheel( event ) {
 
+		//remove default wheel scrool event
+	   	if (event&&event.preventDefault){ 
+	        event.preventDefault();
+	        event.stopPropagation();
+	    }else{ 
+	     	event.returnvalue=false;  
+	     	//return false;     
+	    }
 		if ( scope.enabled === false ) { return; }
 		if ( scope.noZoom === true ) { return; }
 
